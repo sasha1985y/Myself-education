48ac2250b5bdb6f5f4a190eb699bc40ba876bc69
sashaReact <kuyancev1985@mail.ru>
2023-09-18 21:02:34 +0300

    освежил одну из ссылок

--- src/pages/code-pen-screen/code-pen-screen.tsx
+++ src/pages/code-pen-screen/code-pen-screen.tsx
@@ -12,7 +12,7 @@ function CodePenScreen(): JSX.Element {
       <ol className={styles.code_pen_screen_ol}>
         <li><a className={styles.code_pen_screen_a} href="https://codepen.io/sashaNightMare/pen/LYMjrbr?editors=1000">Галерея</a></li>
         <li><a className={styles.code_pen_screen_a} href="https://codepen.io/sashaNightMare/pen/KKbXwyL?editors=0100">Работа с Drag & Drop</a></li>
-        <li><a className={styles.code_pen_screen_a} href="https://codepen.io/frbtmtgv-the-reactor/pen/WNLZEWB">Слайдер</a></li>
+        <li><a className={styles.code_pen_screen_a} href="https://codepen.io/sashaNightMare/pen/LYMzoNz?editors=0010">Слайдер</a></li>
         <li><a className={styles.code_pen_screen_a} href="https://codepen.io/SofiaSergio/pen/qBWXqNW">CSS is awesome!</a></li>
         <li><a className={styles.code_pen_screen_a} href=""></a></li>
         <li><a className={styles.code_pen_screen_a} href=""></a></li>
