1a6fa5ed794c4ec0bc34dcb91a359a3f513062fd
sasha1985y <kuyancev1985@mail.ru>
2025-01-18 21:46:20 +0300

    пытатюсь_5 исправить проблему отображения страницы 404

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -25,7 +25,7 @@ function App(): JSX.Element {
             }
           />
           <Route
-            path="*"
+            path={"*"}
             element={<NotFoundScreen />}
           />
           <Route
